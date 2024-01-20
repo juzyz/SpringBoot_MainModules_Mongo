ec834d836b9f8252078e571e1ad1164cbaa01074
Iuliia Rudi <connection454@gmail.com>
2024-01-20 01:55:22 -0500

    add OpenApiConfig

---
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -0,0 +1,60 @@
+package com.example.demo.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Rudi",
+                        email = "rudisqe@gmail.com",
+                        url = "https://github.com/juzyz"
+                ),
+                title = "Open API /Swagger 3.0",
+                description = "OpenApi documentation for Spring Boot Demo project",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "http://prod.env"
+                )
+        }
+        /* Set security on application level
+        , security = {
+            @SecurityRequirement(
+                    name ="bearerAuthentication"
+            )
+        } */
+)
+
+@SecurityScheme(
+        name = "bearerAuthentication",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+
+
+public class OpenApiConfig {
+}
--- src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -2,12 +2,21 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Student;
 import com.example.demo.services.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
-@RequestMapping( path="api/v1/student")
+@RequestMapping(path = "api/v1/student")
+@Tag(name = "Student Controller API")
+@SecurityRequirement(name = "bearerAuthentication")
 public class StudentController {
 
     private final StudentService studentService;
@@ -17,27 +26,44 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    @Operation(summary = "Fetch all students stored in DB",
+            description = "Some extend description of this endpoint"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Fetch all students from DB",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "403",
+                    description = "Forbidden/ Invalid Token",
+                    content = @Content)
+    })
     @GetMapping
     public List<Student> getStudent() {
         return studentService.getAllStudent();
     }
 
+    @Operation(summary = "Create a new student",
+            description = "Add a new student to DB" )
     @PostMapping
-    public void registerNewStudent(@RequestBody Student student){
-          studentService.addNewStudent(student);
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
     }
 
-    @DeleteMapping(path="{studentId}")
-    public void deleteStudent(@PathVariable("studentId") Long studentId){
-        studentService.deleteStudent (studentId);
+    @Operation(summary = "Delete the student by id",
+            description = "Remove the student from DB")
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 
-    @PutMapping(path="{studentId}")
-    public  void updateStudent(@PathVariable("studentId") Long studentId,
-                               @RequestParam(required = false) String name,  // sent parameter via URL Parameters
-                               @RequestParam(required = false) String email,
-                               @RequestBody Student student  // or parse parameters from json of request body
-                               ){
+    @Operation(summary = "Update the student by id",
+            description = "Update the student in DB")
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId,
+                              @RequestParam(required = false) String name,  // sent parameter via URL Parameters
+                              @RequestParam(required = false) String email,
+                              @RequestBody Student student  // or parse parameters from json of request body
+    ) {
         studentService.updateStudent(studentId, name, email);
     }
 
