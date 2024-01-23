042591f94bc1e7f82baf80e34d24da27a419adca
Iuliia Rudi <connection454@gmail.com>
2024-01-23 01:30:47 -0500

    MongoDb settings: change type of studentId

--- src/main/java/com/example/demo/services/StudentService.java
+++ src/main/java/com/example/demo/services/StudentService.java
@@ -4,6 +4,7 @@ import com.example.demo.exeptions.BadRequestException;
 import com.example.demo.exeptions.StudentNotFoundException;
 import com.example.demo.models.Student;
 import com.example.demo.repositories.StudentRepository;
+import com.example.demo.repositories.sequence.services.SequenceGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,13 @@ import java.util.*;
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
+    private final SequenceGeneratorService seqGeneratorService;
 
     @Autowired
-    public StudentService(StudentRepository studentRepository) {
+    public StudentService(StudentRepository studentRepository, SequenceGeneratorService seqGeneratorService) {
         this.studentRepository = studentRepository;
+        this.seqGeneratorService = seqGeneratorService;
     }
-
 
     public List<Student> getAllStudent() {
         return studentRepository.findAll();
@@ -28,32 +30,33 @@ public class StudentService {
     public void addNewStudent(Student student) {
         Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
         if (studentOptional.isPresent()) {
-            throw new BadRequestException( "Email " + student.getEmail() + " taken");
+            throw new BadRequestException("Email " + student.getEmail() + " taken");
         }
+        student.setId(seqGeneratorService.generateSequence(Student.SEQUENCE_NAME));
         studentRepository.save(student);
     }
 
-    public void deleteStudent(Long studentId) {
+    public void deleteStudent(Integer studentId) {
         studentRepository.findById(studentId)
-                .orElseThrow(()-> new StudentNotFoundException(
+                .orElseThrow(() -> new StudentNotFoundException(
                         "student with id " + studentId + " does not exist"));
         studentRepository.deleteById(studentId);
     }
 
-//    @Transactional // for multiple updates to assure they happen all-or-none.
-    public void updateStudent(@PathVariable("studentId") Long studentId,
+    //    @Transactional // for multiple updates to assure they happen all-or-none.
+    public void updateStudent(@PathVariable("studentId")  Integer studentId,
                               @RequestParam(required = false) String name,
                               @RequestParam(required = false) String email) {
         Student student = studentRepository.findById(studentId)
                 .orElseThrow(() -> new StudentNotFoundException(
                         "student with id " + studentId + " does not exist")
                 );
-        if(name != null && name.length() >0 &&
-                !Objects.equals(student.getName(), name)){
+        if (name != null && name.length() > 0 &&
+                !Objects.equals(student.getName(), name)) {
             student.setName(name);
         }
-        if(email != null && email.length() >0 &&
-                !Objects.equals(student.getEmail(), email)){
+        if (email != null && email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)) {
             student.setEmail(email);
         }
         studentRepository.save(student);
