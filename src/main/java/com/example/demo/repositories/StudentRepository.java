38c73eef096b8a401826e7d99619807a940eea5e
Iuliia Rudi <connection454@gmail.com>
2024-01-24 23:40:20 -0500

    MongoDB: Change Student repository

--- src/main/java/com/example/demo/repositories/StudentRepository.java
+++ src/main/java/com/example/demo/repositories/StudentRepository.java
@@ -1,15 +1,12 @@
 package com.example.demo.repositories;
 
 import com.example.demo.models.Student;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    @Query("SELECT s FROM student s WHERE s.email = ?1")
+public interface StudentRepository extends MongoRepository<Student, Integer> {
     Optional<Student> findStudentByEmail(String email);
-
 }
