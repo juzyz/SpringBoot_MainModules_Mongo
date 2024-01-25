5b70f584821b19570d08bcc36b6ba7ce02d81c2b
Iuliia Rudi <connection454@gmail.com>
2024-01-24 23:39:26 -0500

    Swagger: add description to AuthenticationController

--- src/main/java/com/example/demo/controllers/AuthenticationController.java
+++ src/main/java/com/example/demo/controllers/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.example.demo.jwt.JwtUtils;
 import com.example.demo.config.SecurityConfig;
 import com.example.demo.repositories.UserDoa;
 import com.example.demo.models.AuthenticationRequest;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Import;
@@ -11,10 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Import(SecurityConfig.class)
@@ -25,7 +24,7 @@ public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
     private final UserDoa userDoa;
     private final JwtUtils jwtUtils;
-
+//http://localhost:8080/swagger-ui/index.html
     @PostMapping("/authenticate")
     public ResponseEntity<String> authenticate(
             @RequestBody AuthenticationRequest request) {
@@ -39,6 +38,13 @@ public class AuthenticationController {
         return ResponseEntity.status(400).body("Some Error has occurred.");
     }
 
-
+    @Hidden
+    @Operation( summary="Endpoint to check a controller accessibility",
+            description = "This endpoint is added to demonstrate a work of @Hidden for OpenAPI"
+    )
+    @GetMapping("/say-hello")
+    public ResponseEntity <String> sayHello (){
+        return ResponseEntity.ok("Hello from API");
+    }
 
 }
