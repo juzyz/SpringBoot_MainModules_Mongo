36964632b085e7bca4e2e93020315a9ff5d33ea8
Iuliia Rudi <connection454@gmail.com>
2024-03-19 15:30:40 -0400

    MongoDB integration

--- src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
 
 
 @SpringBootApplication
